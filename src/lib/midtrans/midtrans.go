8e101bcdfd7e9811a49ecc629ac1989fc9063a88
yogipuji15 <yogipuji.yps@gmail.com>
2023-03-10 12:52:44 +0700

    test

--- src/lib/midtrans/midtrans.go
+++ src/lib/midtrans/midtrans.go
@@ -75,8 +75,8 @@ func (m *midtransStruct) CreateTransaction(order entity.Orders,speaker entity.Sp
 	}
 	
 	
-	resp, _ := m.midtransClient.ChargeTransaction(req)
-	return resp,nil
+	resp, err := m.midtransClient.ChargeTransaction(req)
+	return resp,err
 	
 }
 
