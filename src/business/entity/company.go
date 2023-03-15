58e93c1b3124c589f60b45030e871e955c9d64d1
yogipuji15 <yogipuji.yps@gmail.com>
2023-03-15 20:17:28 +0700

    DEPLOY

--- src/business/entity/company.go
+++ src/business/entity/company.go
@@ -7,10 +7,12 @@ type Companies struct {
 	CompanyName 		string `gorm:"type:varchar(255);unique" json:"company"`
 	Email       		string `gorm:"type:varchar(50);unique" json:"email"`
 	Photo 				string `gorm:"type:varchar(255)" json:"photo"`
+	Bio					string `gorm:"longtext" json:"bio"`
 	Description			string `gorm:"longtext" json:"description"`
 	Location 	  		string `gorm:"type:varchar(255)" json:"location"`
 	ProposalsAccepted	int `gorm:"type:int" json:"proposalsAccepted"`
 	Partner				string `gorm:"type:varchar(255)" json:"partner"`
+	Website				string `gorm:"type:varchar(255)" json:"website"`
 	CategoryID			uint `json:"categoryId"`
 	Category 			Categories `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
