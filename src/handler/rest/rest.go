acd1a6f7a309068ca8009f28b9e47f77147f476e
yogipuji15 <yogipuji.yps@gmail.com>
2023-03-16 00:25:07 +0700

    DEPLOY update API

--- src/handler/rest/rest.go
+++ src/handler/rest/rest.go
@@ -27,7 +27,7 @@ func Init(usecase *usecase.Usecase) Rest {
 
 func (r *rest) Run() {
 	r.gin.Use(func(c *gin.Context) {
-		// c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		
 		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, authorization, accept, origin, Cache-Control, X-Requested-With")
