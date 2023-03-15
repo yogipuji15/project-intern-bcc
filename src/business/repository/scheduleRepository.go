b58f2b436b8169408b3a6fd8000c6bf205e317ae
yogipuji15 <yogipuji.yps@gmail.com>
2023-03-15 12:50:15 +0700

    DEPLOY

--- src/business/repository/scheduleRepository.go
+++ src/business/repository/scheduleRepository.go
@@ -7,9 +7,9 @@ import (
 )
 
 type ScheduleRepository interface {
-	Create(schedule entity.Schedules)(entity.Schedules,error)
-	GetAllBySpeakerId(speakerId string,month string)([]entity.Schedules,error)
-	GetById(Id string)(entity.Schedules,error)
+	Create(schedule entity.Schedules) (entity.Schedules, error)
+	GetAllBySpeakerId(speakerId string, month string) ([]entity.Schedules, error)
+	GetById(Id string) (entity.Schedules, error)
 }
 
 type scheduleRepository struct {
@@ -17,22 +17,23 @@ type scheduleRepository struct {
 }
 
 func NewScheduleRepository(db *gorm.DB) ScheduleRepository {
-	return &scheduleRepository{db:db}
+	return &scheduleRepository{db: db}
 }
 
-func (h *scheduleRepository) Create(schedule entity.Schedules)(entity.Schedules,error){
-	err:=h.db.Create(&schedule).Error
-	return schedule,err
+func (h *scheduleRepository) Create(schedule entity.Schedules) (entity.Schedules, error) {
+	err := h.db.Create(&schedule).Error
+	return schedule, err
 }
 
-func (h *scheduleRepository) GetAllBySpeakerId(speakerId string,month string)([]entity.Schedules,error){
+func (h *scheduleRepository) GetAllBySpeakerId(speakerId string, month string) ([]entity.Schedules, error) {
 	var schedules []entity.Schedules
-	err:=h.db.Where("time_start BETWEEN ? AND ?",month+"-01 00:00:00",month+"-31 23:59:00").Find(&schedules, "speaker_id = ?", speakerId).Error
-	return schedules,err
+	err := h.db.Where("time_start BETWEEN ? AND ?", month+"-01 00:00:00", month+"-3 23:59:00").Find(&schedules, "speaker_id = ?", speakerId).Error
+
+	return schedules, err
 }
 
-func (h *scheduleRepository) GetById(Id string)(entity.Schedules,error){
+func (h *scheduleRepository) GetById(Id string) (entity.Schedules, error) {
 	var schedule entity.Schedules
-	err:=h.db.Where("id = ?",Id).Find(&schedule).Error
-	return schedule,err
+	err := h.db.Where("id = ?", Id).Find(&schedule).Error
+	return schedule, err
 }
