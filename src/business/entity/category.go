5cb5143520a4968e42cfeab57ffb524acf86a930
yogipuji15 <yogipuji.yps@gmail.com>
2023-03-17 17:17:12 +0700

    DEPLOY update API

--- src/business/entity/category.go
+++ src/business/entity/category.go
@@ -4,5 +4,6 @@ import "gorm.io/gorm"
 
 type Categories struct {
 	gorm.Model
-	Category string `gorm:"type:varchar(50);unique" json:"Category"`
+	Category string `gorm:"type:varchar(50);unique" json:"category"`
+	Photo	 string `gorm:"type:varchar(100)" json:"photo"`
 }
